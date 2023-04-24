4d2f618088632541681193eb8b16fe97caeff161
Ason <ason.vohuynhson@gmail.com>
2023-04-24 15:28:48 +0700

    Create base entity achitecteure

---
+++ Assets/Script/Achitecture/Entities/EntityComponent.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Achitecture.Entities
+{
+    internal abstract class EntityComponent : MonoBehaviour
+    {
+        public abstract void InitializationComponent(EntityController entityController);
+    }
+}
---
+++ Assets/Script/Achitecture/Entities/EntityController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace Achitecture.Entities
+{
+    internal abstract class EntityController : MonoBehaviour
+    {
+        [SerializeField] private List<EntityComponent> entityComponents;
+
+        public T GetEntityComponent<T>() where T : EntityComponent
+        {
+            foreach(var component in entityComponents)
+            {
+                if(component is T) return (T)component;
+            }
+
+            return null;
+        }
+
+        protected void InitializationComponent()
+        {
+            foreach (var component in entityComponents)
+            {
+                component.InitializationComponent(this);
+            }
+        }
+    }
+}
