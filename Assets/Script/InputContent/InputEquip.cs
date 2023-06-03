5b690e3267c6c0e906831f8145a210d34051f1d3
Ason <ason.vohuynhson@gmail.com>
2023-06-03 21:49:57 +0700

    Add_read_InputEquipment

---
+++ Assets/Script/InputContent/InputEquip.cs
@@ -0,0 +1,38 @@
+﻿using StateContents;
+using UnityEngine.InputSystem;
+
+namespace InputContents
+{
+    internal class InputEquip : InputComponent
+    {
+        StateCore stateCore;
+        BaseState EquipWeapon;
+        BaseState UnEquipWeapon;
+        InputEquipment inputEquip;
+
+        public InputEquip()
+        {
+            inputEquip = new InputEquipment();
+            Init();
+        }
+
+        //public InputEquip(StateCore stateCore, BaseState equipWeapon, BaseState unEquipWeapon)
+        //{
+        //    inputEquip = new InputEquipment();
+        //    EquipWeapon = equipWeapon;
+        //    UnEquipWeapon = unEquipWeapon;
+        //}
+
+        void Init()
+        {
+            inputEquip.Equipment.Enable();
+            inputEquip.Equipment.Equip.started += ReadInpuEquipment;
+        }
+
+        void ReadInpuEquipment(InputAction.CallbackContext callback)
+        {
+            UnityEngine.Debug.Log("2002");
+        }
+
+    }
+}
