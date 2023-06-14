69442cc7b550e9accd48bcde7711ffa23f2f9934
Ason <ason.vohuynhson@gmail.com>
2023-06-14 14:50:15 +0700

    Frost_Strike

---
+++ Assets/Script/SkillVFXContents/ScytheSkillVFX/ScytheFrostSpikeVFX.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace ScytheSkillVFX
+{
+    internal class ScytheFrostSpikeVFX : MonoBehaviour
+    {
+        [SerializeField] ParticleSystem _frostSpike;
+        [SerializeField] Transform _target;
+
+        void SummonFrostSpike()
+        {
+            var  positionTarget = _target.position;
+            positionTarget.y = 1;
+            _frostSpike.transform.position = positionTarget;
+            _frostSpike.gameObject.SetActive(true);
+            Invoke(nameof(End), 1f);
+        }
+
+        void End()
+        {
+            _frostSpike.Stop();
+            _frostSpike.gameObject.SetActive(false);
+        }
+    }
+}
