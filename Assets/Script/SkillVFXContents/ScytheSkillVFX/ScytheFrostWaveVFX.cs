b37153bf53c252a79a9544556134197efbced9c3
Ason <ason.vohuynhson@gmail.com>
2023-06-14 14:19:36 +0700

    FrostWave_VFX

---
+++ Assets/Script/SkillVFXContents/ScytheSkillVFX/ScytheFrostWaveVFX.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace ScytheSkillVFX
+{
+    internal class ScytheFrostWaveVFX : MonoBehaviour
+    {
+        [SerializeField] ParticleSystem _frostWave;
+        [SerializeField] Transform _characterSummon;
+        [SerializeField] Vector3 _offsetPosition;
+        [System.Obsolete]
+        void SummonFrostWave()
+        {
+            _frostWave.transform.localPosition = _offsetPosition;
+            _frostWave.transform.parent = null;
+            _frostWave.gameObject.SetActive(true);
+            _frostWave.Play();
+            Invoke(nameof(EndFrostWave), 1f);
+        }
+
+        void EndFrostWave()
+        {
+            _frostWave.gameObject.SetActive(false);
+            _frostWave.transform.parent = _characterSummon;
+        }
+    }
+}
