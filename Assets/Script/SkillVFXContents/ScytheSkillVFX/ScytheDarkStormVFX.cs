56e83d02c86ef884d2831c309b8d0979e2e9970d
Ason <ason.vohuynhson@gmail.com>
2023-06-14 13:38:54 +0700

    DarkStom_SkillEffect

---
+++ Assets/Script/SkillVFXContents/ScytheSkillVFX/ScytheDarkStormVFX.cs
@@ -0,0 +1,33 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+namespace ScytheSkillVFX
+{
+    internal class ScytheDarkStormVFX : MonoBehaviour
+    {
+        [SerializeField] ParticleSystem _darkStorm;
+        [SerializeField] Transform _characterBody;
+        void DarkStormSummon()
+        {
+            _darkStorm.Play();
+            _darkStorm.gameObject.SetActive(true);
+        }
+        IEnumerator FollowPlayer()
+        {
+            while(true)
+            {
+                if(_darkStorm.isStopped)
+                {
+                    yield return null;
+                }
+                _darkStorm.transform.position = Vector3.Lerp(_darkStorm.transform.position, _characterBody.position, Time.deltaTime * 10f);
+            }
+        }
+        void StopDarkStorm()
+        {
+            _darkStorm.gameObject.SetActive(false);
+            _darkStorm.Stop();
+        }
+    }
+}
