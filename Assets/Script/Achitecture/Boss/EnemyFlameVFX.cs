3ebfb51d8b840c591742f927f19b5f37d2a6261d
Ason <ason.vohuynhson@gmail.com>
2023-06-17 23:30:50 +0700

    EnemyVFX

---
+++ Assets/Script/Achitecture/Boss/EnemyFlameVFX.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+public class EnemyFlameVFX : MonoBehaviour
+{
+    [SerializeField] ParticleSystem _flame;
+    
+    void SummonFlame()
+    {
+        _flame.gameObject.SetActive(true);
+        _flame.Play();
+    }
+
+    void StopFlame()
+    {
+        _flame.gameObject.SetActive(false);
+        _flame.Stop();
+    }
+        
+}
